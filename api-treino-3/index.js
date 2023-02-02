d44ff4740f5aabd7f4993a18bb5763404fc26f35
lucianoscabora <lucianoscabora@gmail.com>
2023-02-02 03:20:11 -0300

    refactor and css adjust

--- api-treino-3/index.js
+++ api-treino-3/index.js
@@ -1,31 +1,13 @@
-import { Url } from "./src/variables.js";
-const searchInput = document.getElementById('search-input');
+import { getJobs } from "./src/scripts/jobs.js";
+import { getProfile } from "./src/scripts/character.js";
 const searchBtn = document.getElementById('search-btn');
-
-async function getProfile(characterId) {
-    const response = await fetch(`${Url}${characterId}`);
-    const data = await response.json();
-    console.log(data);
-    const avatar = data.Character.Portrait;
-    const fc = data.Character.FreeCompanyName;
-    const name = data.Character.Name;
-    const classes = [data.Character.ActiveClassJob.UnlockedState.Name]
-
-    let userInfo = `    <img src="${avatar}" class="image-info" alt="imagem x" /> 
-                        <h1>Nome do personagem: ${name}</h1>
-                        <h2>Classe ativa: ${classes}</h2>  
-                        <div class="fc-information"> 
-                        <h3>Free Company: ${fc}</h3>
-                        </div>
-    `
-    console.log(userInfo)
-    document.querySelector('.main-container').innerHTML += userInfo;
-}
 
 searchBtn.addEventListener('click', () =>  {
     const characterId = document.getElementById('search-input').value
-    getProfile(characterId); 
+    getProfile(characterId);
+    getJobs(characterId);
 })
+
 
 
 
